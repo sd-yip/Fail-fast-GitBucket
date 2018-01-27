fbdf30e887ac79293731623258cf7d795ed0a5c3
Nicholas Yip <yiplunkwun@gmail.com>
2018-01-28 07:52:33 +0900

    Download GitBucket

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import de.undercouch.gradle.tasks.download.Download
+
 plugins {
   id("de.undercouch.download") version "3.3.0"
 }
@@ -7,5 +9,12 @@ buildscript {
   }
   dependencies {
     classpath("org.ow2.asm:asm:5.2")
+  }
+}
+
+tasks {
+  "download"(Download::class) {
+    src("https://github.com/gitbucket/gitbucket/releases/download/4.21.2/gitbucket.war")
+    dest(".")
   }
 }
