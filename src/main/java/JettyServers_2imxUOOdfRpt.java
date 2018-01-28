5ee58874b9aeca434b21556865a66371e99917ad
Nicholas Yip <yiplunkwun@gmail.com>
2018-01-28 21:42:33 +0900

    '>>>>>> Start server'

--- src/main/java/JettyServers_2imxUOOdfRpt.java
+++ src/main/java/JettyServers_2imxUOOdfRpt.java
@@ -2,12 +2,14 @@ import org.eclipse.jetty.server.Server;
 
 public class JettyServers_2imxUOOdfRpt {
   public static void startOrExitJvm(Server server) {
+    System.out.println(">>>>>> Start server");
+
     try {
       server.start();
-      System.out.println("Server started successfully");
+      System.out.println(">>>>>> Server started successfully");
     }
     catch (Throwable t) {
-      System.err.println("Error encountered during server start");
+      System.err.println(">>>>>> Error encountered during server start");
       t.printStackTrace();
       System.exit(132);
     }
