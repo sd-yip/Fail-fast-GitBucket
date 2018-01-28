4fa1f885426a0368b9cae2025de6770f9265451d
Nicholas Yip <yiplunkwun@gmail.com>
2018-01-28 22:06:15 +0900

    Apply SLF4J

--- src/main/java/JettyServers_2imxUOOdfRpt.java
+++ src/main/java/JettyServers_2imxUOOdfRpt.java
@@ -1,16 +1,19 @@
 import org.eclipse.jetty.server.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JettyServers_2imxUOOdfRpt {
+  private static final Logger logger = LoggerFactory.getLogger(JettyServers_2imxUOOdfRpt.class);
+
   public static void startOrExitJvm(Server server) {
-    System.out.println(">>>>>> Start server");
+    logger.info(">>>>>> Start server");
 
     try {
       server.start();
-      System.out.println(">>>>>> Server started successfully");
+      logger.info(">>>>>> Server started successfully");
     }
     catch (Throwable t) {
-      System.err.println(">>>>>> Error encountered during server start");
-      t.printStackTrace();
+      logger.error(">>>>>> Error encountered during server start", t);
       System.exit(132);
     }
   }
