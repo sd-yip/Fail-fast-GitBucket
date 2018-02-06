1734a8ced05b8c72a8d1825ddf2d2a9b0597fdc3
Nicholas Yip <yiplunkwun@gmail.com>
2018-02-07 01:03:20 +0900

    Detect failures during the context initialization

--- src/main/java/JettyServers_2imxUOOdfRpt.java
+++ src/main/java/JettyServers_2imxUOOdfRpt.java
@@ -1,16 +1,27 @@
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerWrapper;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 public class JettyServers_2imxUOOdfRpt {
   public static void startOrExitJvm(Server server) {
     System.out.println(">>>>>> Start server");
 
     try {
+      WebAppContext wac = (WebAppContext) ((HandlerWrapper) server.getHandler()).getHandler();
+      wac.setThrowUnavailableOnStartupException(true);
+
       server.start();
       System.out.println(">>>>>> Server started successfully");
     }
     catch (Throwable t) {
       System.err.println(">>>>>> Error encountered during server start");
       t.printStackTrace();
+
+      try {
+        Thread.sleep(750);
+      }
+      catch (InterruptedException ignored) {
+      }
       System.exit(132);
     }
   }
